ac09c0986b05ccc46f47981edaf04a7dfcb82fcc
Kunal Arora <90236283+kunalarora0930@users.noreply.github.com>
2023-08-03 22:13:10 +0530

    created frontend Homepage

---
+++ fu-ctional/frontend/src/components/AnimatedCards.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import "./AnimatedCards.css"
+
+const AnimatedCards = () => {
+    return (
+        <div className='marqueeContainer'>
+
+        <marquee>
+            <div className='animatedCardsContainer'>
+
+                <div class="card">
+                    Magic Card
+                </div>
+                <div class="card">
+                    Magic Card
+                </div>
+                <div class="card">
+                    Magic Card
+                </div>
+                <div class="card">
+                    Magic Card
+                </div>
+                <div class="card">
+                    Magic Card
+                </div>
+                <div class="card">
+                    Magic Card
+                </div>
+                <div class="card">
+                    Magic Card
+                </div>
+                <div class="card">
+                    Magic Card
+                </div>
+                <div class="card">
+                    Magic Card
+                </div>
+                <div class="card">
+                    Magic Card
+                </div>
+
+            </div>
+        </marquee>
+        </div>
+    )
+}
+
+export default AnimatedCards
